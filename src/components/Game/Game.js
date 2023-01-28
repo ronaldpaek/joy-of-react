502ce4f0866027971be8552b4329541093e6c274
Ronald Paek <ronaldpaek@gmail.com>
2023-01-27 17:13:41 -0800

    Create Guess component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -13,7 +13,7 @@ console.info({ answer });
 
 function Game() {
     const [guesses, setGuesses] = React.useState([]);
-    
+
   return (
     <>
       <GuessResults guesses={guesses} />
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,42 +1,42 @@
 import React from "react";
 
 function GuessInput({ guesses, setGuesses }) {
-    const [guess, setGuess] = React.useState("");
+  const [guess, setGuess] = React.useState("");
 
-    function handleSubmit(event) {
-      event.preventDefault();
+  function handleSubmit(event) {
+    event.preventDefault();
 
-      if (guess.length !== 5) return;
+    if (guess.length !== 5) return;
 
-      const newGuess = {
-        guess,
-        id: Date.now(),
-      };
-      const nextGuesses = [...guesses, newGuess];
+    const newGuess = {
+      guess,
+      id: Date.now(),
+    };
+    const nextGuesses = [...guesses, newGuess];
 
-      setGuesses(nextGuesses);
-      setGuess("");
-    }
+    setGuesses(nextGuesses);
+    setGuess("");
+  }
 
-    function handleGuessChange(event) {
-      const newGuess = event.target.value.toUpperCase();
+  function handleGuessChange(event) {
+    const newGuess = event.target.value.toUpperCase();
 
-      if (newGuess.length > 5) return;
+    if (newGuess.length > 5) return;
 
-      setGuess(newGuess);
-    }
+    setGuess(newGuess);
+  }
 
-    return (
-      <form className="guess-input-wrapper" onSubmit={handleSubmit}>
-        <label htmlFor="guess-input">Enter guess:</label>
-        <input
-          id="guess-input"
-          type="text"
-          value={guess}
-          onChange={handleGuessChange}
-        />
-      </form>
-    );
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={handleGuessChange}
+      />
+    </form>
+  );
 }
 
 export default GuessInput;
